e85caf1bdbada42e7f16839b525ecdaf22e454ac
zrael0 <richmondlao1999@gmail.com>
2025-08-12 12:17:07 -0400

    add start screen and pause function

--- main.py
+++ main.py
@@ -17,12 +17,18 @@ def main():
     Player.containers = (updatable, drawable)
     Shot.containers = (updatable, drawable, shots)
 
+    game_start = False
+    pause = False
     asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     font = pygame.font.SysFont(None, 24)
+    big_font = pygame.font.SysFont(None, 50)
     dt = 0
+
+    def clear_screen():
+        pygame.Surface.fill(screen, (0,0,0))
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -32,24 +38,39 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
-        updatable.update(dt)
-        for asteroid in asteroids:
-            if asteroid.collide_with(player):
-                print("Game over!")
-                sys.exit()
-            for shot in shots:
-                if shot.collide_with(asteroid):
-                    shot.kill()
-                    asteroid.split()
-        for obj in drawable:
-            obj.draw(screen)
+        keys = pygame.key.get_pressed()
+        if not game_start:
+            clear_screen() 
+            if keys[pygame.K_SPACE]:
+                game_start = True
+            start_text = big_font.render("Press SPACE to start", True, (200,200,200))
+            screen.blit(start_text, (480,340))
+        if pause:
+            clear_screen()
+            pause_text = big_font.render("Paused", True, (200,200,200))
+            screen.blit(pause_text, (580,340))
+            if keys[pygame.K_SPACE]:
+                pause = False
+        elif game_start:
+            clear_screen() 
+            updatable.update(dt)
+            for asteroid in asteroids:
+                if asteroid.collide_with(player):
+                    print("Game over!")
+                    sys.exit()
+                for shot in shots:
+                    if shot.collide_with(asteroid):
+                        shot.kill()
+                        asteroid.split()
+            for obj in drawable:
+                obj.draw(screen)
 
-        fps = clock.get_fps()
-        fps_text = font.render(f"{fps:.2f} FPS", True, (50,255,50)) 
-        screen.blit(fps_text, (10,10))
-
-        dt = clock.tick(60)/1000.0
+            fps = clock.get_fps()
+            fps_text = font.render(f"{fps:.2f} FPS", True, (50,255,50)) 
+            screen.blit(fps_text, FPS_TEXT)
+            dt = clock.tick(60)/1000.0
+            if keys[pygame.K_p]:
+                pause = True
         pygame.display.flip()
 
 if __name__ == "__main__":
